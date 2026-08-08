b4a64784fbed38ed7997d5f6d35f2429d98bdc2f
viktorsirotin <vxod@sirotin.eu>
2026-08-08 14:51:46 +0200

    struct: Skills-restructuring. Part 2

--- ts/ts-stop/test/sfsm/tutorial/06-case-study-turnstile-sfsm/sfsm/model/use-cases/uc-0-system-initialization/tests/start-test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Re-run the test for use case 0: System initialization
-cd "$(dirname "$0")/.."
-node ../../../../../../../ts/ts-stop/scripts/run-fa.js ../../turnstile-behavior.json run-01/signals.txt run-01/output-trace.txt run-01/commands.json
